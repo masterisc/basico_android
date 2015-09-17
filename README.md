f34ed001542b2ee2ec177f6558281f19dc227ac7
Sebastian Moreno <sebastian@chainpeople.com>
2015-09-17 18:37:24 -0300

    carga de codigos de ejemplo basico android

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# basico_android
-Ejemplos básicos de Android del canal de youtube appmovilchile
+Tutorial de Android Studio para Aplicaciones Móviles Chile
+
+PARTE 1 - El primero de los tutoriales de aprendiendo a programar aplicaciones móviles en Android Studio donde aprenderemos a realizar nuestra primera aplicación utilizando los elementos TextView y Button.
+
+PARTE 2 - En este tutorial aprenderemos a capturar el input del usuario ingresado en un campo EditText y lo desplegaremos en un elemento Toast.
+
+PARTE 3 - En este tutorial aprenderemos a crear una nueva Activity, movernos de una Activity a otra al presionar un botón y traspasar información entre Activities.
---
+++ parte3/Activity/app/src/main/java/com/appmovilchile/activity/MainActivity.java
@@ -0,0 +1,66 @@
+package com.appmovilchile.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button boton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        boton = (Button) findViewById(R.id.goToActivity);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                //codigo a ejecutar
+
+                String informacionExtra = "Hola Mundo";
+
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+
+                intent.putExtra("extra", informacionExtra);
+
+                startActivity(intent);
+
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
